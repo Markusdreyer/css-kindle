c964c41beedd414bee2428213c62f0842d9c3e5b
Markus Dreyer <markus.a.dreyer@gmail.com>
2018-01-15 13:37:55 +0100

    added plant_page

---
+++ plant_page/scripts.js
@@ -0,0 +1,61 @@
+var valueElement;
+var gauge;
+
+//Oppgave 1: Legg inn URL til json-data feeden i ajax-kallet.
+//Oppgave 2: Definer grenseverdier for våt, fuktig og tørr utifra hva din sensor leser inn. Gjør dette i displayValue
+
+
+$(document).ready(function(){
+
+		valueElement = document.getElementById("value");
+
+		gauge = new JustGage({
+		    id: "gauge",
+		    value: 0,
+		    min: 0,
+		    max: 1024,
+		    title: "Fuktighet",
+		    levelColors: ["#a52a2a", "#00ff00", "#0080ff"]
+		  });
+
+
+		getMoistureLevel();
+
+	setInterval(function(){
+		getMoistureLevel();
+	}, 15000);
+	
+
+});
+	
+	function getMoistureLevel(){
+		$.ajax({
+	  		url: "http://phant.labben.org:8090/output/mZKoqNL9m1frqxNKjGKxu0EVKYz.json"})
+			.done(function(data) {
+			  	displayValue(data[0].moisture)
+			});
+			setTimeout(getMoistureLevel, 30000);
+	}
+
+
+	function displayValue(value){
+
+			if(value > 350){
+				valueElement.innerHTML = "våt";
+			}
+			else if(value <= 350 && value > 150){
+				valueElement.innerHTML = "fuktig";
+			}else if(value <= 150){
+				valueElement.innerHTML = "tørr";
+			}
+
+			updateGauge(value);
+		}
+
+
+
+
+	function updateGauge(value){
+		
+		gauge.refresh(value);
+	}
